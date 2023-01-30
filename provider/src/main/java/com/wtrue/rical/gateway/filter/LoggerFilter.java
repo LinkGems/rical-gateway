50e9d435813e41bd1c9bdf1cb1da23f204647a27
luzelong <382491212@qq.com>
2023-01-30 19:19:34 +0800

    美化日志打印

--- provider/src/main/java/com/wtrue/rical/gateway/filter/LoggerFilter.java
+++ provider/src/main/java/com/wtrue/rical/gateway/filter/LoggerFilter.java
@@ -4,7 +4,10 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
@@ -22,16 +25,23 @@ import java.net.URI;
 public class LoggerFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        // 获取请求信息
+        // 1.获取请求信息
         ServerHttpRequest request = exchange.getRequest();
         InetSocketAddress address = request.getRemoteAddress();
         String method = request.getMethodValue();
         URI uri = request.getURI();
-        HttpHeaders headers = request.getHeaders();
+        HttpHeaders requestHeaders = request.getHeaders();
         // 获取请求query
         MultiValueMap<String, String> map = request.getQueryParams();
+        log.info("LoggerFilter.filter , request come in, please look down： \n{\n\trequest = {} \n\taddress = {} \n\tmethod = {} \n\turi = {} \n\trequestHeaders = {} \n\tmap = {}\n}",request,address,method,uri,requestHeaders,map);
 
-        log.info("LoggerFilter.filter , come in! 具体请求细节请往下看： \n request = {} \n address = {} \n method = {} \n uri = {} \n headers = {} \n map = {}",request,address,method,uri,headers,map);
+        // 2.获取响应信息
+        ServerHttpResponse response = exchange.getResponse();
+        HttpStatus statusCode = response.getStatusCode();
+        MultiValueMap<String, ResponseCookie> cookies = response.getCookies();
+        HttpHeaders responseHeaders = response.getHeaders();
+        log.info("LoggerFilter.filter , response is : \n{\n\tstatusCode = {} \n\tcookies = {} \n\tresponseHeaders = {}\n}",statusCode,cookies,responseHeaders);
+
 
         return chain.filter(exchange);
     }
